34a3d5a39787094b2fb763a8d45db32399670240
aranjan2 <atul.prasad.333@gmail.com>
2021-06-12 09:21:47 +0530

    cli fix

--- fs-cli/src/main/java/com/woven/Main.java
+++ fs-cli/src/main/java/com/woven/Main.java
@@ -1,11 +1,25 @@
 package com.woven;
 
 import com.woven.cli.FileSystemCli;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import picocli.CommandLine;
 
+@SpringBootApplication
 public class Main {
+
+  public static String[] sargs;
+
   public static void main(String[] args) {
-    int exitCode = new CommandLine(new FileSystemCli()).execute(args);
+    sargs = args;
+    var app = SpringApplication.run(Main.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  void run(ApplicationReadyEvent event) {
+    int exitCode = new CommandLine(new FileSystemCli()).execute(sargs);
     System.exit(exitCode);
   }
 }
--- fs-cli/src/main/java/com/woven/command/impl/WebFSCommandImpl.java
+++ fs-cli/src/main/java/com/woven/command/impl/WebFSCommandImpl.java
@@ -1,53 +1,53 @@
 package com.woven.command.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.woven.command.FSCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import com.woven.command.FSCommand;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 @Component
 public class WebFSCommandImpl implements FSCommand {
   private WebClient webClient;
+  Logger logger = LoggerFactory.getLogger(WebFSCommandImpl.class);
 
   public WebFSCommandImpl() {
-    this.webClient = WebClient.builder().baseUrl(System.getenv("FS_SERVER_BASE")).build(); ;
+    this.webClient = WebClient.builder().baseUrl(System.getenv("FS_SERVER_BASE")).build();
   }
 
-  Logger logger = Logger.getLogger(String.valueOf(WebFSCommandImpl.class));
   final static RestTemplate restTemplate = new RestTemplate();
 
   @Override
   public void delete(String fileName) {
-    logger.log(Level.FINE,"Deleting file" + fileName);
+    logger.debug("Deleting file {}", fileName);
 
   }
 
   @Override
   public void upload(String fileName) {
-    logger.log(Level.FINE,"uploading file" + fileName);
+    logger.debug("uploading file {}", fileName);
 
-    webClient.post()
-            .uri("http://localhost:8080/uploadFile")
-            .contentType(MediaType.MULTIPART_FORM_DATA)
-            .body(BodyInserters.fromMultipartData(fromFile(new File("HELP.md"))))
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
+    try {
+      webClient.post()
+              .uri("http://localhost:8080/uploadFile")
+              .contentType(MediaType.MULTIPART_FORM_DATA)
+              .body(BodyInserters.fromMultipartData(fromFile(new File("HELP.md"))))
+              .retrieve()
+              .bodyToMono(String.class)
+              .block();
+    } catch (Exception e) {
+      System.out.println("Error Occured :" + e.getMessage());
+    }
   }
 
   public MultiValueMap<String, HttpEntity<?>> fromFile(File file) {
@@ -58,6 +58,6 @@ public class WebFSCommandImpl implements FSCommand {
 
   @Override
   public void list(String pathPrefix) {
-    logger.log(Level.FINE,"listing contents:");
+    logger.debug("listing contents:");
   }
 }
